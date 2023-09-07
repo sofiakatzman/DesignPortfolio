1b7c9e8166a0be6b8f42550e4bdb98ec5c66f34f
Sofia Katzman <katzmansof@gmail.com>
2023-09-07 09:40:07 -0400

    content updates for design

--- src/components/Pages/Design/Design.jsx
+++ src/components/Pages/Design/Design.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './Design.css'
+import Technologies from '../Technologies/Technologies'
 
 
 function Design(){
@@ -8,8 +9,9 @@ function Design(){
             <button className="design-category">USER INTERFACE</button> <br/>
             <button className="design-category">DIGITAL</button> <br/>
             <button className="design-category">PRINT</button> <br/>
+            <button className="design-category">PROJECTS</button> <br/>
             <button className="design-category">ILLUSTRATIONS</button> <br/>
-
+            {/* <Technologies /> */}
         </div>
     )
 }
