eabc0fbeda0d20ab97da9068b0f61e7b65e34bd6
Sofia Katzman <katzmansof@gmail.com>
2023-09-10 17:41:15 -0400

    edit about page

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -1,29 +1,24 @@
 import React from "react"
 import './About.css'
 import headshot from '../../../assets/headshot.jpg'
-import resume from '../../../assets/Sofia_Katzman_Design.pdf'
+
 import Contact from '../../Layout/Contact/Contact'
 
-function About(){
-
-    return(
-        <div className="about">
-            <div className="about-container">
-
-                <div className="about-blurb">
-                    <h2>Designer and Creator</h2>
-                    <h3>New York, NY </h3>
-                    <p>Hello! I'm Sofia - I design and build things.</p>
-                    <p>From crafting eye-catching design portfolios to bringing creative concepts to life, I thrive in the realm of visual storytelling and artistic innovation. With a diverse background encompassing design, project management, and creative production, I'm passionate about transforming ideas into tangible works of art.</p>  
+function About() {
+    return (
+        <div className="home">
+            <div className="home-container">
+                <div className="home-blurb">
+                    <h2>Sofia Katzman</h2>
+                    <h3>Artist and Designer</h3>
+                    <p>Hello! I'm Sofia, and my passion lies in bringing imaginative concepts to life.</p>
+                    <p>With a diverse background that spans design, project management, and creative production, I specialize in the art of visual storytelling and innovative design. <br/><br/>I'm on a mission to turn ideas into tangible works of art and memorable experiences.</p>
                     <br/><br/><br/>
-                    <a href={resume} download className="resume">Download My Resume</a>
+                    
                 </div>
-                
             </div>
         </div>
-
-  
-    )
+    );
 }
 
 export default About
