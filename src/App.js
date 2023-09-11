d251b86e5b4cfaac93e727f35781a5fd029ab652
Sofia Katzman <katzmansof@gmail.com>
2023-09-11 02:10:04 -0400

    remove large files

--- src/App.js
+++ src/App.js
@@ -2,29 +2,19 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Footer from "./components/Layout/Footer/Footer"
 import Home from "./components/Pages/Home/Home"
-import Projects from "./components/Pages/Projects/Projects"
 import ContactMe from './components/Pages/ContactMe/ContactMe'
 import ProjectDetail from './components/Pages/Projects/ProjectsDetail/ProjectDetail'
-import Design from './components/Pages/Design/Design'
-import UserInterface from './components/Pages/Design/UserInterface/UserInterface'
-import ClientProjects from './components/Pages/Design/ClientProjects/ClientProjects'
 import ClientProjectDetail from './components/Pages/Design/ClientProjects/ClientProjectDetail/ClientProjectDetail'
-import Digital from './components/Pages/Design/UserInterface/Digital/Digital'
 import AboutMe from './components/Pages/AboutMe/AboutMe'
 import Gallery from './components/Pages/Gallery/Gallery'
+
 function App() {
   return (
     <div className="app">
       <Router>
-               
         <Routes>
-          {/* <Route path="/projects" element={<Projects />} /> */}
           <Route path="/design" element={<Gallery />} /> 
-          {/* paths for all project pages will go here */}
-            <Route path="/design/ui" element={<UserInterface />} /> 
-            <Route path="/design/clientprojects" element={<ClientProjects />} />
             <Route path="/design/clientprojects/:name" element={<ClientProjectDetail />} />
-            <Route path="/design/digital" element={<Digital />} />
           <Route path="/project/:name" element={<ProjectDetail />} />
           <Route path="/aboutme" element={<AboutMe />} /> 
           <Route path="/contactme" element={<ContactMe />} />
