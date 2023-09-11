c7013c89794cc2f2e71caa7cf242b99b6a48d9ef
Sofia Katzman <katzmansof@gmail.com>
2023-09-10 21:46:54 -0400

    update gallery content

--- src/components/Pages/Gallery/Gallery.jsx
+++ src/components/Pages/Gallery/Gallery.jsx
@@ -3,57 +3,124 @@ import './Gallery.css'
 import ImageGalleryDisplay from "./ImageGalleryDisplay/ImageGalleryDisplay";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-import GALLERYDATA from "./GalleryData"; // Import your data structure here
+import {ARTAPPIMAGES, INVITEMUSIC, INVITEBIRTHDAY, INVITEART, INVITESPORT, TUCANO, BAETAILS, ENTERTAINMENTACCESS} from "./GalleryData"; // Import your data structure here
 
 function Gallery() {
-  const [activeTab, setActiveTab] = useState(0);
-  const [activeSubtab, setActiveSubtab] = useState(0);
-
-  const handleTabClick = (tabIndex) => {
-    setActiveTab(tabIndex);
-    setActiveSubtab(0); // Reset subtab when changing main tabs
-  };
-
-  const handleSubtabClick = (subtabIndex) => {
-    setActiveSubtab(subtabIndex);
-  };
-
-  const currentTabData = GALLERYDATA[activeTab];
-  const currentSubtabData = currentTabData.subtabs[activeSubtab];
-
   return (
-    <div className="design-categories">
+
+    // <div className="design-categories">
+    //   <Tabs forceRenderTabPanel defaultIndex={0}>
+    //     <TabList>
+    //       {GALLERYDATA.map((tab, index) => (
+    //         <Tab key={index} onClick={() => handleTabClick(index)}>
+    //           {tab.label}
+    //         </Tab>
+    //       ))}
+    //     </TabList>
+    //     {GALLERYDATA.map((tab, index) => (
+    //       <TabPanel key={index}>
+    //         <Tabs forceRenderTabPanel defaultIndex={0}>
+    //           <TabList>
+    //             {tab.subtabs.map((subtab, subtabIndex) => (
+    //               <Tab
+    //                 key={subtabIndex}
+    //                 onClick={() => handleSubtabClick(subtabIndex)}
+    //               >
+    //                 {subtab.label}
+    //               </Tab>
+    //             ))}
+    //           </TabList>
+    //           <TabPanel>
+    //             <ImageGalleryDisplay images={currentSubtabData.images} />
+    //             {/* You can access the class name from the subtab's label */}
+    //             <p>Class Name: {currentSubtabData.label}</p>
+    //           </TabPanel>
+    //         </Tabs>
+    //       </TabPanel>
+    //     ))}
+    //   </Tabs>
+    // </div>
+<div className="design-categories">
       <Tabs forceRenderTabPanel defaultIndex={0}>
+
+       {/* MAIN TABS */}
         <TabList>
-          {GALLERYDATA.map((tab, index) => (
-            <Tab key={index} onClick={() => handleTabClick(index)}>
-              {tab.label}
-            </Tab>
-          ))}
+          <Tab>User Interface</Tab>
+          <Tab>Invitations</Tab>
+          <Tab>Client Projects</Tab>
         </TabList>
-        {GALLERYDATA.map((tab, index) => (
-          <TabPanel key={index}>
-            <Tabs forceRenderTabPanel defaultIndex={0}>
-              <TabList>
-                {tab.subtabs.map((subtab, subtabIndex) => (
-                  <Tab
-                    key={subtabIndex}
-                    onClick={() => handleSubtabClick(subtabIndex)}
-                  >
-                    {subtab.label}
-                  </Tab>
-                ))}
-              </TabList>
-              <TabPanel>
-                <h2>Subtab Name: {currentSubtabData.label}</h2>
-                <ImageGalleryDisplay images={currentSubtabData.images} />
-                <p>Class Name: {currentSubtabData.images[0]?.className}</p>
-              </TabPanel>
-            </Tabs>
-          </TabPanel>
-        ))}
+
+       {/* UI TAB */}
+        <TabPanel>
+          <Tabs forceRenderTabPanel>
+            <TabList>
+              <Tab>The Art App</Tab>
+            </TabList>
+            <TabPanel>
+              <ImageGalleryDisplay images={ARTAPPIMAGES}/>
+            </TabPanel>
+          </Tabs>
+        </TabPanel>
+
+        {/* Invites TAB  */}
+        <TabPanel>
+          <Tabs forceRenderTabPanel>
+            <TabList>
+              <Tab>Music</Tab>
+              <Tab>Birthday</Tab>
+              <Tab>Art</Tab>
+              <Tab>Sport</Tab>
+            </TabList>
+            {/* Invites: Music */}
+                <TabPanel>
+                  <ImageGalleryDisplay images={INVITEMUSIC}/>
+                </TabPanel>
+                {/* Invites: Birthday */}
+                <TabPanel>
+                  <ImageGalleryDisplay images={INVITEBIRTHDAY}/>
+                </TabPanel>
+                {/* Invites: Art */}
+                <TabPanel>
+                  <ImageGalleryDisplay images={INVITEART}/>
+                </TabPanel>
+                {/* Invites: Sport */}
+                <TabPanel>
+                <ImageGalleryDisplay images={INVITESPORT}/>
+                </TabPanel>
+          </Tabs>
+        </TabPanel>
+
+        {/* CLIENT PROJECTS TAB  */}
+        <TabPanel>
+          <Tabs forceRenderTabPanel>
+            <TabList>
+              <Tab>Cafe Tucano</Tab>
+              <Tab>Bae Tails</Tab>
+              <Tab>Entertainment Access</Tab>
+              {/* <Tab>Rombiolo</Tab> */}
+            </TabList>
+            {/* Invites: Tucano */}
+                <TabPanel>
+                  <ImageGalleryDisplay images={TUCANO}/>
+                </TabPanel>
+                {/* Invites: BaeTails */}
+                <TabPanel>
+                  <ImageGalleryDisplay images={BAETAILS}/>
+                </TabPanel>
+                {/* Invites: EA */}
+                <TabPanel>
+                  <ImageGalleryDisplay images={ENTERTAINMENTACCESS}/>
+                </TabPanel>
+                {/* Invites: Rombiolo */}
+                {/* <TabPanel>
+                <ImageGalleryDisplay images={INVITESPORT}/>
+                </TabPanel> */}
+          </Tabs>
+        </TabPanel>
+
+        
       </Tabs>
-    </div>
+      </div>
   );
 }
 
--- src/components/Pages/Gallery/GalleryData.js
+++ src/components/Pages/Gallery/GalleryData.js
@@ -1,46 +1,163 @@
-const GALLERYDATA = [
-    {
-      label: "USER INTERFACE",
-      subtabs: [
-        {
-          label: "Subtab 1",
-          images: [
-            {
-              original: "https://picsum.photos/id/1018/1000/600/",
-              className: "ui",
-            },
-            // Add more images for Subtab 1 as needed
-          ],
-        },
-        {
-          label: "Subtab 2",
-          images: [
-            {
-              original: "https://picsum.photos/id/1019/1000/600/",
-              className: "ui",
-            },
-            // Add more images for Subtab 2 as needed
-          ],
-        },
-      ],
-    },
-    {
-      label: "DIGITAL",
-      subtabs: [
-        {
-          label: "Subtab 1",
-          images: [
-            {
-              original: "https://picsum.photos/id/1015/1000/600/",
-              className: "digital",
-            },
-            // Add more images for Subtab 1 as needed
-          ],
-        },
-        // Add more subtabs for DIGITAL as needed
-      ],
-    },
-    // Add more main tabs as needed
-  ];
+export const ARTAPPIMAGES = [
+  {
+    original: require('./GalleryAssets/ArtApp/0ArtAppiPhoneMockup.png'),
+    className: "ui",
+  },
+  {
+    original: require('./GalleryAssets/ArtApp/1ArtAppHomeScreen.png'),
+    className: "ui",
+  },
+  {
+    original: require('./GalleryAssets/ArtApp/2ArtAppFilter.png'),
+    className: "ui",
+  },
+  {
+    original: require('./GalleryAssets/ArtApp/3ArtAppArtist.png'),
+    className: "ui",
+  },
+  {
+    original: require('./GalleryAssets/ArtApp/4ArtAppExhibitionInsights.png'),
+    className: "ui",
+  },
+  {
+    original: require('./GalleryAssets/ArtApp/ArtAppSavedArtistandSearches.png'),
+    className: "ui",
+  },
+]
 
-  export default GALLERYDATA
+export const INVITEMUSIC = [
+  {
+    original: require('./GalleryAssets/Invites/MartinezBrothersEventInvite.png'),
+  },
+  {
+    original: require('./GalleryAssets/Invites/GuyGerberEventInvite.png'),
+  },
+  {
+    original: require('./GalleryAssets/Invites/FogozinhoEventInvite.png'),
+  },
+  {
+    original: require('./GalleryAssets/Invites/GuyGerber2EventInvite.png'),
+  },
+]
+
+export const INVITEBIRTHDAY = [
+  {
+    original: require('./GalleryAssets/Invites/ChloeBdayEventInvite.png'),
+  },
+  {
+    original: require('./GalleryAssets/Invites/HarriBdayEventInvite.png'),
+  },
+  {
+    original: require('./GalleryAssets/Invites/LuizaEventInvite.png'),
+  },
+  {
+    original: require('./GalleryAssets/Invites/MonicaBaddarEventInvite.png'),
+  },
+  {
+    original: require('./GalleryAssets/Invites/Chloe2EventInvite.png'),
+  }
+]
+
+export const INVITEART = [
+  {
+    original: require('./GalleryAssets/Invites/HalfGalleryEventInvite.png'),
+  },
+  {
+    original: require('./GalleryAssets/Invites/EmilyFergusonEventInvite.png'),
+  },
+  {
+    original: require('./GalleryAssets/Invites/NathanRitterpuschEventInvite.png'),
+  },
+  {
+    original: require('./GalleryAssets/Invites/YuangFangEventInvite.png'),
+  },
+  
+  
+]
+
+export const INVITESPORT = [
+  {
+    original: require('./GalleryAssets/Invites/MaroonMadnessEventInvite.png'),
+  },
+]
+
+export const TUCANO = [
+  {
+    original: require('./GalleryAssets/Tucano/TucanoBrunchFront.png'),
+  },
+  {
+    original: require('./GalleryAssets/Tucano/TucanoBrunchBack.png'),
+  },
+  {
+    original: require('./GalleryAssets/Tucano/Tucano Full Menu.png'),
+  },
+  {
+    original: require('./GalleryAssets/Tucano/fascadedesign.PNG'),
+  },
+  {
+    original: require('./GalleryAssets/Tucano/tucanofascade.jpg'),
+  },
+  {
+    original: require('./GalleryAssets/Tucano/tucanomural.JPG'),
+  },
+  {
+    original: require('./GalleryAssets/Tucano/mosaic1.jpg'),
+  },
+  {
+    original: require('./GalleryAssets/Tucano/mosaic2.JPG'),
+  },
+  {
+    original: require('./GalleryAssets/Tucano/mosaic3.JPG'),
+  },
+  {
+    original: require('./GalleryAssets/Tucano/tucanoinvite1.jpg'),
+  },
+  {
+    original: require('./GalleryAssets/Tucano/tucanoinvite2.jpg'),
+  },
+  
+]
+
+export const BAETAILS = [
+  {
+    original: require('./GalleryAssets/BaeTails/BaeTailsBuisnessCardMockup.png'),
+  },
+  {
+    original: require('./GalleryAssets/BaeTails/BaeTailsBrochureMockup.png'),
+  },
+  {
+    original: require('./GalleryAssets/BaeTails/BaeTailsStickers.png'),
+  },
+  {
+    original: require('./GalleryAssets/BaeTails/bt-backmural.png'),
+  },
+  {
+    original: require('./GalleryAssets/BaeTails/bt-fascade.png'),
+  },
+  {
+    original: require('./GalleryAssets/BaeTails/bt-lobby.png'),
+  },
+  {
+    original: require('./GalleryAssets/BaeTails/bt-logo.jpg'),
+  },
+  // {
+  //   original: require('./GalleryAssets/BaeTails/BaeTailsColorPalette.png'),
+  // },
+
+  
+]
+
+export const ENTERTAINMENTACCESS = [
+  {
+    original: require('./GalleryAssets/EntertainmentAccess/EntertainmentAccessTShirt.png'),
+  },
+  {
+    original: require('./GalleryAssets/EntertainmentAccess/EntertainmentAccessBadges.png'),
+  },
+
+  // {
+  //   original: require('./GalleryAssets/BaeTails/BaeTailsColorPalette.png'),
+  // },
+
+  
+]
